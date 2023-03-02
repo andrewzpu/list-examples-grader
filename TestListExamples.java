fe3bdd416731771ccbc7413700c00d24453a1b64
andrewzpu <66804382+andrewzpu@users.noreply.github.com>
2023-03-02 15:07:54 -0800

    added tester file

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+
+import java.beans.Transient;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,12 +12,32 @@ class IsMoon implements StringChecker {
 }
 
 public class TestListExamples {
-  @Test(timeout = 500)
-  public void testMergeRightEnd() {
-    List<String> left = Arrays.asList("a", "b", "c");
-    List<String> right = Arrays.asList("a", "d");
-    List<String> merged = ListExamples.merge(left, right);
-    List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
-    assertEquals(expected, merged);
-  }
+    @Test(timeout = 500)
+    public void testMergeRightEnd() {
+      List<String> left = Arrays.asList("a", "b", "c");
+      List<String> right = Arrays.asList("a", "d");
+      List<String> merged = ListExamples.merge(left, right);
+      List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
+      assertEquals(expected, merged);
+    }
+
+    @Test(timeout = 500)
+    public void testFilter() {
+        StringChecker ismoon = new IsMoon();
+        List<String> input = Arrays.asList("moon","test","Moon");
+        List<String> filtered = ListExamples.filter(input,ismoon);
+        List<String> expected = Arrays.asList("moon","Moon");
+        assertEquals(expected, filtered);
+
+    }
+
+    @Test
+    public void testTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testagain() {
+        assertTrue(true);
+    }
 }
